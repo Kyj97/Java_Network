1ec987f80a78acf5e0029dc7293ccffafd4399df
Kyj97 <gld66777@gmail.com>
2023-02-14 13:39:27 +0900

    real

--- src/day21/SimpleEchoServer.java
+++ src/day21/SimpleEchoServer.java
@@ -1,55 +1,49 @@
 package day21;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
+// IP 165.246.115.165 포트 20000
 
-public class SimpleEchoServer {
+public class SimpleEchoServer implements Runnable {
+    // 다중 접속 에코 서버
+    private static Socket clientSocket;
+    public SimpleEchoServer(Socket clientSocket)
+    {
+        this.clientSocket = clientSocket;
+    }
     public static void main(String[] args) {
-        System.out.println("에코 서버 시작됨");
-        try (ServerSocket serverSocket = new ServerSocket(9000)) {
-            System.out.println("클라이언트 접속 대기 중.....");
-            Socket clientSocket = serverSocket.accept();  // 접속 대기
-            System.out.println("클라이언트 접속됨.");
-
-            BufferedReader br = null;
-            PrintWriter pw = null;
-            try {
-                br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                pw = new PrintWriter(clientSocket.getOutputStream(), true);
-//                String line;
-//                while ((line = br.readLine()) != null) {
-//                    System.out.println("클라이언트로 부터 받은 메세지 : " + line);
-//                    pw.println(line);  // 클라이언트로 송신
-//                }
-                Supplier<String> socketIn = () -> {
-                    try {
-                        return br.readLine();
-                    } catch (IOException ex) {
-                        return null;
-                    }
-                };
-                Stream s = Stream.generate(soketIn);
-                s.map(text -> {
-                    System.out.println("클라이언트로부터 받은 메세지");
-                    pw.println(text);
-                    return text;
-                }).allMatch(t -> )
-
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } finally {
-                br.close();
-                pw.close();
+        System.out.println("다중 접속 에코 서버");
+        try (ServerSocket serverSocket = new ServerSocket(20000)) {
+            while (true) {
+                System.out.println("클라이언트 접속 대기 중.....");
+                clientSocket = serverSocket.accept();
+                SimpleEchoServer tes = new SimpleEchoServer(clientSocket);
+                new Thread(tes).start();
             }
+        } catch (IOException ex) {
+
         }
-        catch (IOException ex) {
-            System.out.println("접속 실패!");
+        System.out.println("다중 접속 에코 서버 종료");
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread() + " 스레드 접속");
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
+        ) {
+            String inputLine;
+            while ((inputLine = br.readLine()) != null) {
+                System.out.println(Thread.currentThread() +" 클라이언트가 보낸 메세지 : " + inputLine);
+                out.println(inputLine);
+            }
+            System.out.println(Thread.currentThread() +" 클라이언트가 종료됨"); }
+        catch (IOException ex)
+        {
         }
     }
 }
--- src/day22/TimeClient.java
+++ src/day22/TimeClient.java
@@ -1,29 +1,34 @@
 package day22;
-
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
 public class TimeClient {
+    // MultiCast Client
     public static void main(String args[]) {
         System.out.println("멀티 캐스트 타임 클라이언트");
-        try (MulticastSocket socket = new MulticastSocket(10000)) {
-            InetAddress group = InetAddress.getByName("224.0.0.7");
+        MulticastSocket socket = null;
+        try{
+            socket = new MulticastSocket(10000);
+            InetAddress group = InetAddress.getByName("224.0.0.117");
             socket.joinGroup(group);
             System.out.println("멀티캐스트 타임 서버 그룹에 조인 완료");
             byte[] buffer = new byte[256];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length); //서버쪽은 인수많은데 여긴인수 두개
-            for (int i = 0; i < 5; i++) {
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            for (int i = 0; i < 10; i++) {
                 socket.receive(packet);
                 String received = new String(packet.getData());
                 System.out.println(received.trim());
             }
-            socket.leaveGroup(group);
-        } catch (IOException ex) {
-// Handle exception
+            socket.leaveGroup(group);  // 멀티캐스트 타임 서버 탈퇴
         }
-        System.out.println("Multicast Time Client Terminated");
+        catch (IOException ex) {
+            System.out.println("입출력 예외 발생!");
+        }
+        finally {
+            if(socket != null) socket.close();
+        }
+        System.out.println("멀티 캐스트 타임 클라이언트 종료됨");
     }
 }
-
